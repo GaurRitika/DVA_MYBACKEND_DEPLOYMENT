221739af4c013b6ead5e284d34ff08efc49002ba
kuritika098 <ritika525.hd@gmail.com>
2025-05-31 16:11:41 +0530

    Pushing mybackend code's for render deployment

---
+++ routes/Auth.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+import { Login, 
+    // Logout
+     Register } from '../controllers/Auth.js'
+// import upload from '../middleware/Multer.js'
+
+
+const AuthRoutes = express.Router()
+
+
+AuthRoutes.post('/register' , Register)
+AuthRoutes.post('/login',Login)
+// AuthRoutes.post('/logout',Logout)
+export default AuthRoutes 
